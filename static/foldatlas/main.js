316fda66a93ccb983fb0b00951e038c9ffdecc1c
Matthew Norris (JIC) <matthew.norris@jic.ac.uk>
2015-05-15 17:04:56 +0100

    Improved the reactivity chart

--- static/foldatlas/main.js
+++ static/foldatlas/main.js
@@ -11,23 +11,23 @@ function BrowserController(config) {
 	// 	this.staticBase = window.browserControllerConfig;
 	// },
 
+	this.init = function() {
+		instance.drawReactivities();
+	}
+
 	this.selectTranscript = function(transcriptID) {
 		$("#transcript-loading").show()
 		this.changeUrl(transcriptID, "/transcript/"+transcriptID)
-		$("#transcript-data").load("/ajax/transcript/"+transcriptID, function() {
+
+		$.ajax({
+			url: "/ajax/transcript/"+transcriptID,
+			context: this
+		}).done(function(results) {
 			$("#transcript-loading").hide();
-
-			// not the best way to do it...
-			var json = $.parseJSON($("#reactivities-json").html())
-
-			console.log(json)
-
-			if (json.length != 0) {
-				instance.drawReactivities(json);
-			} else {
-				alert("No reactivity data found");
-			}
-		})
+			$("#transcript-data").empty();
+			$("#transcript-data").html(results);
+			instance.drawReactivities();
+		});
 	}
 
 	this.changeUrl = function(title, url) {
@@ -41,18 +41,24 @@ function BrowserController(config) {
 
 	this.drawReactivities = function(reactivities) {
 
-		var data = reactivities
+		var data = this.getReactivitiesJson()
 
 		// Define chart dimensions including axis margins
-		var margin = {left: 80, bottom: 80}
-		var width = 900 - margin.left, height = 250 - margin.bottom;
+		var margin = {top: 20, right: 20, bottom: 40, left: 40}
+		var	width = 898 - margin.left - margin.right,
+			height = 248 - margin.bottom - margin.top;
 
 		// Define the scales
 		var x = d3.scale.linear()
-		    .range([0, width]);
+			// range maps to the pixel dimensions.
+		    .range([0, width], .1) 
+
+		    // domain describes the range of data to show.
+		    .domain([0, d3.max(data, function(d) { return d.position; })]);
 
 		var y = d3.scale.linear()
-		    .range([height, 0]);
+		    .range([height, 0])
+		    .domain([0, d3.max(data, function(d) { return d.reactivity; })]);
 
 	   	// Create axis objects
 		var xAxis = d3.svg.axis()
@@ -63,34 +69,33 @@ function BrowserController(config) {
 		var yAxis = d3.svg.axis()
 		    .scale(y)
 		    .orient("left")
-    		.ticks(10);
+    		.ticks(5); // how many ticks to show.
 
 		// Define bar width, depends on n values and also width of canvas
 		var barWidth = width / data.length;
-		console.log("barWidth: "+barWidth);
 
 		// Get the chart's container element
 		var chart = d3.select("#reactivities-chart")
-			.attr("width", width + margin.left)
-			.attr("height", height + margin.bottom)
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.bottom + margin.top)
 
 		// Add x-axis objects to the chart
 		chart.append("g")
 			.attr("class", "x axis")
-			.attr("transform", "translate("+margin.left+"," + height + ")")
+			.attr("transform", "translate("+margin.left+"," + (height + margin.top) + ")")
 			.call(xAxis);
 
 		// Add y-axis objects to the chart
 		chart.append("g")
 			.attr("class", "y axis")
-			.attr("transform", "translate("+margin.left+", 0)")
+			.attr("transform", "translate("+margin.left+", "+margin.top+")")
 			.call(yAxis)
 
 		// Add bar elements to the chart
 		var bar = chart.selectAll("g")
 			.data(data)
 			.enter().append("g")
-			.attr("transform", function(d, i) { return "translate(" + (margin.left + (i * barWidth)) + ",0)"; })
+			.attr("transform", function(d, i) { return "translate(" + (margin.left + (i * barWidth)) + ", "+margin.top+")"; })
 
 		// Add rectangles to the bars
 		bar.append("rect")
@@ -110,6 +115,13 @@ function BrowserController(config) {
 		//   return d;
 		// }
 	}
+
+	this.getReactivitiesJson = function() {
+		var json = $.parseJSON($("#reactivities-json").html())
+		return json
+	}
+
+	this.init()
 }
 
 $(document).ready(function () { 
@@ -118,6 +130,6 @@ $(document).ready(function () {
 });
 
 $("#chromosome-selector").change(function() {
-	// TODO reload the page .. difficult / impossible to change the chromosome
+	// TODO reload the page .. difficult / impossible to change the chromosome 
 	// ... 
 });
