a59a02b6a49499f63f4bd73bae830a507a71fdc2
Matthew Norris (JIC) <matthew.norris@jic.ac.uk>
2015-07-01 18:56:24 +0100

    Figured out PCA, now need to decompose structures into 1s and 0s

--- foldatlas/structure.py
+++ foldatlas/structure.py
@@ -4,19 +4,23 @@
 from sklearn import decomposition
 
 data = [
-	[0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1],
-	[0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1],
-	[0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
-	[0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1],
-	[0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
-	[0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1],
-	[0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1],
-	[1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0],
-	[1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1],
+	[0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+	[0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1],
+	[1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+	[1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 1],
+	[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1],
+	[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+	[1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1],
 	[1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1]
 ]
 
 def do_pca(data):
+
+	# hmm seems to work. BUT how do you know which entries are which?
+	# Simple - results always listed in the order that they were added.
+
 	pca = decomposition.PCA(n_components=2)
 	pca.fit(data)
 	results = pca.transform(data)
@@ -24,6 +28,11 @@ def do_pca(data):
 	print(results)
 
 do_pca(data);
+
+# from sklearn import datasets
+# iris = datasets.load_iris()
+# do_pca(iris.data);
+# print(iris.target)
 
 
 
