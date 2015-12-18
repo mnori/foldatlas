2ba4e0758c04bebcd1dcde78e99605d0b9460abf
Matthew Norris <matthew.norris@jic.ac.uk>
2015-12-18 02:18:17 +0000

    Monitor now checks and emails

--- foldatlas/monitor.py
+++ foldatlas/monitor.py
@@ -1,20 +1,32 @@
+import traceback
 import os
+import urllib.request  # the lib that handles the url stuff
 
-# must call "sudo apt-get install sendmail" first...
+test_url = "http://www.foldatlas.com/transcript/AT2G45180.1"
+recipient = "matthew.gs.norris@gmail.com"
+search_str = "AT2G45180.1"
 
+def run_test():
+	try:	
+		data = urllib.request.urlopen(test_url) # it's a file like object and works just like a file
+		text = str(data.read())
 
-# if sts != 0:
-#     print("Sendmail exit status "+str(sts))
+		if search_str in text:
+			print("It worked!")
+		else:
+			send_error(text)
+	except:
+		send_error(traceback.format_exc())
 
-
-
-def send_error(recipient, error_details):
+def send_error(error_details):
+	print("FAILED")
 	SENDMAIL = "/usr/sbin/sendmail" # sendmail location
 
 	p = os.popen("%s -t" % SENDMAIL, "w")
 	p.write("To: "+recipient+"\n")
 	p.write("Subject: FoldAtlas error\n")
 	p.write("\n") # blank line separating headers from body
-	p.write("Some text\n")
-	p.write("some more text\n")
+	p.write(error_details)
 	sts = p.close()
+
+run_test()
